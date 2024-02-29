84660b1dc9b3869b7492d40f2437608a201c700f
BananaJam <vladbyn@gmail.com>
2024-02-29 16:01:12 +0200

    added most of main page

---
+++ client/src/components/navbar.js
@@ -0,0 +1,29 @@
+import "./navbar.css";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="logo-section">
+                <img src="/logo192.png" alt="logo" className="logo" />
+                <h1><a href="/" className="logo-text">Ration</a></h1>
+            </div>
+            <ul className="links">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About Us</a>
+                </li>
+                <li>
+                    <a href="/blog">Blog</a>
+                </li>
+                <li>
+                    <a href="/recipes">Recipe</a>
+                </li>
+                <li>
+                    <a href="/login" className="login-button">Log In</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
