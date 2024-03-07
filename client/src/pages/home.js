aaead86744d2b4f53a54d91dc77cf44924df0803
BananaJam <vladbyn@gmail.com>
2024-03-07 15:38:20 +0200

    added search bar and unfinished filter group on ingredients page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,6 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-
 import "./home.css"
 import Rating from "../components/rating"
 import Navbar from '../components/navbar';
@@ -106,57 +103,8 @@ export default function Home() {
                         </div>
                     </div>
                 </section>
-
-                <footer>
-                    <div className="footer-top">
-                        <div className="footer-left">
-                            <div className="footer-logo">
-                                <img src="/icons/logo.svg" alt="logo" />
-                                <h1>Ration</h1>
-                            </div>
-                            <h3>+34953495098</h3>
-                            <p>support@ration.agency</p>
-                        </div>
-                        <div className="footer-center">
-                            Quick links
-                            <ul>
-                                <li>Home</li>
-                                <li>Plans</li>
-                                <li>Recipes</li>
-                                <li>Blog</li>
-                            </ul>
-                        </div>
-
-                        <div className="footer-right">
-                            <h3>Subscribe to our newsletter</h3>
-                            <input type="text" placeholder="Enter your email" />
-                            <button><FontAwesomeIcon icon={faArrowRight} /></button>
-                        </div>
-                        
-                    </div>
-
-                    <hr />
-
-                    <div className="footer-bottom">
-                        <div className="footer-social">
-                            <div className="social-icon">
-                                <img src="/icons/instagram.svg" alt="instagram" />
-                            </div>
-                            <div className="social-icon">
-                                <img src="/icons/linkedin.svg" alt="instagram" />
-                            </div>
-                            <div className="social-icon">
-                                <img src="/icons/twitter.svg" alt="instagram" />
-                            </div>
-                        </div>
-                        <div className="footer-logo">
-                                <img src="/icons/logo.svg" alt="logo" />
-                                <h1>Ration</h1>
-                            </div>
-                        <p>© 2021 Ration. All rights reserved.</p>
-                    </div>
-                </footer>
             </div>
+            <Footer />
         </>
     );
 }
