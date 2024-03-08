446181f1cebd00faa64ff27151a95a26e2118a55
Nieszczesliwy <zborivskyiworkingspace@gmail.com>
2024-03-08 23:08:09 +0200

    Added views

--- server/products/views.py
+++ server/products/views.py
@@ -6,3 +6,11 @@ from products.serializers import ProductSerializer
 class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def list(self, request, *args, **kwargs):
+        response = super(ProductListView, self).list(request, *args, **kwargs)
+        response.data = {
+            'message': 'Список продуктів успішно отримано.',
+            'products': response.data
+        }
+        return response
