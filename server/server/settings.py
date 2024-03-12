a740cfafd22db0fe04893371e5e277b906d21bda
Nieszczesliwy <zborivskyiworkingspace@gmail.com>
2024-03-12 21:27:47 +0200

    Add CORS for React server

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,17 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # Дозволяє доступ для розробницького сервера React
+#     "https://example.com",  # Додайте домен вашого продакшн-фронтенду
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True #дозволити доступ з усіх доменів
 
 ROOT_URLCONF = "server.urls"
 
