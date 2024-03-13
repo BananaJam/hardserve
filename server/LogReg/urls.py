45e42a36d45bc6ead7bf60c0d52ce7da570ba426
Jorge Zborivskyi <zborivskyi@Jorges-MacBook-Air.local>
2024-03-13 20:36:36 +0200

    Added a password reset

--- server/LogReg/urls.py
+++ server/LogReg/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserRegistrationView, LoginView
+from .views import UserRegistrationView, LoginView, PasswordResetRequestView, PasswordResetConfirmView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', LoginView.as_view(), name='user-login'),
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
--- server/LogReg/views.py
+++ server/LogReg/views.py
@@ -17,6 +17,8 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.urls import reverse
 from django.core.mail import send_mail
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.template.loader import render_to_string
 
 User = get_user_model()
 
@@ -51,7 +53,7 @@ class UserRegistrationView(APIView):
                 try:
                     send_mail(
                         'Підтвердіть вашу електронну пошту',
-                        'Натисніть на посилання нижче, щоб підтвердити вашу електронну пошту:\nhttp://your-website.com/confirm/' + str(user.confirmation_token),
+                        'Натисніть на посилання нижче, щоб підтвердити вашу електронну пошту:\nhttp://localhost:8000/confirm/' + str(user.confirmation_token),
                         'your-email@gmail.com',
                         [user.email],
                         fail_silently=False,
@@ -62,3 +64,43 @@ class UserRegistrationView(APIView):
                 return Response({"message": "Користувач успішно зареєстрований. Будь ласка, підтвердіть вашу електронну пошту."}, status=status.HTTP_201_CREATED)
             else:
                 return Response({"message": "Недійсні дані.", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+class PasswordResetRequestView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        user = User.objects.filter(email=email).first()
+        if not user:
+            return Response({"message": "Користувача з такою електронною поштою не знайдено."}, status=status.HTTP_404_NOT_FOUND)
+
+        token_generator = PasswordResetTokenGenerator()
+        token = token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+
+        mail_subject = 'Скидання пароля'
+        message = 'Вітаю {0},\n\nВи надіслали запит на скидання пароля. Натисніть посилання нижче, щоб скинути пароль:\n\nhttp://localhost:3000/accounts/password-reset-confirm/{1}/{2}'.format(user.username, uid, token)
+
+        try:
+            send_mail(mail_subject, message, 'your-email@gmail.com', [user.email])
+        except Exception as e:
+            return Response({"message": "Помилка при відправці електронної пошти.", "error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return Response({"message": "Посилання для скидання пароля було відправлено на вашу електронну пошту."}, status=status.HTTP_200_OK)
+
+class PasswordResetConfirmView(APIView):
+    def post(self, request, uidb64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and PasswordResetTokenGenerator().check_token(user, token):
+            new_password = request.data.get('new_password')
+            if new_password:
+                user.set_password(new_password)
+                user.save()
+                return Response({"message": "Пароль успішно скинуто."}, status=status.HTTP_200_OK)
+            else:
+                return Response({"message": "Новий пароль не надано."}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"message": "Посилання для скидання пароля недійсне."}, status=status.HTTP_400_BAD_REQUEST)
