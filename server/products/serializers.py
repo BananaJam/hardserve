9d860228f269e3760cbdd1f22436603381cbf32a
Thundem <vovkandrij7@gmail.com>
2024-03-14 22:49:58 +0200

    Added Nutriens and ProductNutriens serializers

--- server/products/serializers.py
+++ server/products/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Nutriens, Product_Nutriens
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['name', 'image', 'category', 'short_description', 'proteins', 'fats', 'carbohydrates', 'calories', 'glycemic_index']
+        fields = ['name', 'image', 'category', 'short_description', 'glycemic_index']
+
+class NutriensSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nutriens
+        fields = ['name', 'unit']
+
+class ProductNutriensSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    nutrient = NutriensSerializer(read_only=True)
+
+    class Meta:
+        model = Product_Nutriens
+        fields = ['product', 'nutrient', 'amount']
