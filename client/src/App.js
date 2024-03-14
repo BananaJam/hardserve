3aa7173b18e615e7076ca10461b5e075e992cdf0
Andrii Pentsak <andriypentsak59b@gmail.com>
2024-03-14 10:49:49 +0200

    Pages Log in, Sign Up Front

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LogIn from "./components/LogIn/LogIn";
+import SignUp from "./components/SignUp/SignUp";
+import Navbar from "./components/LogIn/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+        <Navbar />
+
+        <Routes>
+
+          <Route path="/" element={<LogIn/>} />
+          <Route path="/sign" element={<SignUp />} />
+
+        </Routes>
+
+      </Router>
     </div>
   );
 }
