a9ed9e8e92f2b0f0964e688f99da39cd811d4c54
Thundem <vovkandrij7@gmail.com>
2024-03-14 22:51:43 +0200

    Added new path for ProductNutriensListView

--- server/products/urls.py
+++ server/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from products.views import ProductListView
+from products.views import ProductListView, ProductNutriensListView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product-list'),
+    path('<int:product_id>/', ProductNutriensListView.as_view(), name='product-nutrients-by-product-id'),
 ]
