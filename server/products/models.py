92c8e02733fea20a1723532d1bc1b763be79ce8b
Thundem <vovkandrij7@gmail.com>
2024-03-14 22:48:17 +0200

    added models for Notriens and Product_Nutriens

--- server/products/models.py
+++ server/products/models.py
@@ -4,13 +4,24 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=255, null=True)
     short_description = models.TextField(blank=True, null=True)
-    proteins = models.DecimalField(max_digits=5, decimal_places=2)
-    fats = models.DecimalField(max_digits=5, decimal_places=2)
-    carbohydrates = models.DecimalField(max_digits=5, decimal_places=2)
-    calories = models.DecimalField(max_digits=5, decimal_places=2)
     glycemic_index = models.IntegerField()
     image = models.ImageField(upload_to='products/image/', null=True, blank=True)
-    # Додайте інші поля 
 
     def __str__(self):
         return self.name
+    
+class Nutriens(models.Model):
+    name = models.CharField(max_length=255)
+    unit = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product_Nutriens(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_nutrients')
+    nutrient = models.ForeignKey(Nutriens, on_delete=models.CASCADE, related_name='product_nutrients')
+    amount = models.DecimalField(max_digits=10, decimal_places=5)
+
+    def __str__(self):
+        return self.product.name + ' ' + self.nutrient.name
