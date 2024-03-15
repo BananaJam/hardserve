fb5d2556f0d2f855f7ba30e925d3b11ee74e1c5d
Thundem <vovkandrij7@gmail.com>
2024-03-15 16:39:59 +0200

    added fields for main nutriens to ProductSerializer

--- server/products/serializers.py
+++ server/products/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from .models import Product, Nutriens, Product_Nutriens
 
 class ProductSerializer(serializers.ModelSerializer):
+    proteins = serializers.FloatField(read_only=True)
+    carbs = serializers.FloatField(read_only=True)
+    fats = serializers.FloatField(read_only=True)
+    calories = serializers.FloatField(read_only=True)
+
     class Meta:
         model = Product
-        fields = ['name', 'image', 'category', 'short_description', 'glycemic_index']
+        fields = ['name', 'image', 'category', 'short_description', 'proteins', 'fats', 'carbs', 'calories','glycemic_index']
 
 class NutriensSerializer(serializers.ModelSerializer):
     class Meta:
