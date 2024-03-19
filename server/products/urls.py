0ce9bed791f789bab9836201a6d47733f62bb8c8
Владислав Вибираний <31660416+BananaJam@users.noreply.github.com>
2024-03-19 21:21:48 +0200

    Revert "Scrum 68 (adding items in database)"

--- server/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from products.views import ProductListView, ProductNutriensListView, GetImageByName
-
-urlpatterns = [
-    path('', ProductListView.as_view(), name='product-list'),
-    path('<int:product_id>/', ProductNutriensListView.as_view(), name='product-nutrients-by-product-id'),
-    path('images/<str:image_name>/', GetImageByName, name='get-image-by-name')
-]   
