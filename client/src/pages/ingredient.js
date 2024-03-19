228b15e73dcf0e97b9a7a661daa952870a70ced8
Владислав Вибираний <31660416+BananaJam@users.noreply.github.com>
2024-03-19 14:26:05 +0200

    ingredients page with demo data

--- client/src/pages/ingredient.js
+++ client/src/pages/ingredient.js
@@ -1,5 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark, faScaleUnbalanced } from "@fortawesome/free-solid-svg-icons";
 import { useParams } from "react-router-dom";
 import "./ingredient.css";
+
+import Nav from "../components/navbar";
 
 const demoIngredients = [
     {name: "Ingredient 1", category: "Vegetable", proteins: 50, fats: 20, carbs: 30, fiber: 10, calories: 100, vitamins: ["A", "B", "C"], allergens: ["Nuts", "Milk"]},
@@ -20,34 +24,93 @@ const demoIngredients = [
     {name: "Ingredient 8", category: "Spices", proteins: 50, fats: 20, carbs: 30, fiber: 10, calories: 100, vitamins: ["A", "B", "C"], allergens: ["Nuts", "Milk"]},
 ];
 
+
+
+function RecipeCard({recipe}) {
+    return (
+        <div className="flex flex-col items-center gap-2 p-2 bg-gray-200 rounded-lg shadow-lg">
+            <img className="rounded-lg" src={recipe.imgSrc} alt="Recipe" />
+            <div>
+                <h4 className="text-lg font-bold">{recipe.name}</h4>
+                <p>{recipe.description}</p>
+            </div>
+        </div>
+    )
+}
+
+function IngredientCard({product}) {
+    return (
+        <div className="flex flex-col items-center gap-2 p-2 bg-gray-200 rounded-lg shadow-lg">
+            <img className="rounded-lg" src={product.imgSrc} alt="Product" />
+            <div>
+                <h4 className="text-lg font-bold">{product.name}</h4>
+                <p>{product.description}</p>
+            </div>
+        </div>
+    )
+}
+
 export default function Ingredient() {
     const { id: ingredientId } = useParams();
     const ingredient = demoIngredients[ingredientId];
     return (
-        <div className="relative flex flex-row items-center p-4 mx-48 my-16 bg-white border-2 shadow-xl min-w-fit rounded-xl">
-            <img className="m-2 rounded h-96" src="https://via.placeholder.com/150" alt="Ingredient" />
-            <div className="flex flex-col items-center justify-around w-full h-96">
-                <div className="flex gap-2">
-                    <h3 className="text-5xl font-bold">{ingredient.name}</h3>
-                    <p className="bg-orange-500  macro-badge-big">{ingredient.category}</p>
-                </div>
-            
-                <div className="p-2 macros">
-                    <div className="flex gap-2">
-                        <p className="bg-green-400 macro-badge-big">P: {ingredient.proteins}g</p>
-                        <p className="macro-badge-big bg-sky-400">F: {ingredient.fats}g</p>
-                        <p className="macro-badge-big bg-amber-300">C: {ingredient.carbs}g</p>
+        <>
+        <Nav />
+        <div className="flex justify-center ">
+            <div className="relative flex flex-col items-center my-16 bg-white border-2 shadow-xl w-[80vw] min-w-fit rounded-xl">
+                <div className="flex flex-col items-center w-full md:items-start md:flex-row h-fit">
+                    <img className="object-contain m-2 rounded h-96" src="https://via.placeholder.com/150" alt="Ingredient" />
+                    <div className="flex flex-col items-center justify-around w-full h-96">
+                        <div>
+                            <div className="flex items-center gap-2">
+                                <h3 className="text-5xl font-bold">{ingredient.name}</h3>
+                                <p className="bg-orange-500 macro-badge-big">{ingredient.category}</p>
+                            </div>
+                        
+                            <div className="p-2 macros">
+                            <div className="flex justify-center gap-2 pt-2">
+                                    <p className="macro-badge-big bg-violet-400">Fiber: {ingredient.fiber}g</p>
+                                    <p className="macro-badge-big bg-fuchsia-400">Calories: {ingredient.calories}kcal</p>
+                                </div>
+                                <div className="flex justify-center gap-2 pt-2">
+                                    <p className="bg-green-400 macro-badge-big">P: {ingredient.proteins}g</p>
+                                    <p className="macro-badge-big bg-sky-400">F: {ingredient.fats}g</p>
+                                    <p className="macro-badge-big bg-amber-300">C: {ingredient.carbs}g</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full p-2 bg-gray-200 rounded-lg shadow-inner max-w-96">
+                            <h3 className="text-2xl font-bold text-center">Nutritional Information</h3>
+                            <p>Product description</p>
+                            <p>Vitamins: {ingredient.vitamins?.join(", ")}</p>
+                            <p>Allergens: {ingredient.allergens?.join(", ")}</p>
+                        </div>
                     </div>
-                    <div className="flex justify-center gap-2 pt-2">
-                        <p className="macro-badge-big bg-violet-400">Fiber: {ingredient.fiber}g</p>
-                        <p className="macro-badge-big bg-fuchsia-400">Calories: {ingredient.calories}kcal</p>
+
+                    <div className="flex flex-row items-center gap-4 p-4 w-min md:flex-col">
+                        <FontAwesomeIcon className="h-6 hover:text-rose-500 hover:cursor-pointer" icon={faBookmark} />
+                        <FontAwesomeIcon className="h-6 hover:text-green-500 hover:cursor-pointer" icon={faScaleUnbalanced} />
                     </div>
                 </div>
-                <div className="w-full p-2 bg-gray-200 rounded-lg shadow-inner max-w-96">
-                    <p>Vitamins: {ingredient.vitamins?.join(", ")}</p>
-                    <p>Allergens: {ingredient.allergens?.join(", ")}</p>
+
+                <div className="p-4 w-max">
+                    <h3 className="mb-4 text-2xl font-bold text-center">Recipes with this ingredient</h3>
+                    <div className="flex flex-row flex-wrap gap-2">
+                    {RecipeCard({recipe: {name: "Recipe 1", description: "Description 1", imgSrc: "https://via.placeholder.com/150"}})}
+                    {RecipeCard({recipe: {name: "Recipe 2", description: "Description 2", imgSrc: "https://via.placeholder.com/150"}})}
+                    {RecipeCard({recipe: {name: "Recipe 3", description: "Description 3", imgSrc: "https://via.placeholder.com/150"}})}
+                    </div>
+                </div>
+
+                <div className="p-4 w-max">
+                    <h3 className="mb-4 text-2xl font-bold text-center">Similar ingredients</h3>
+                    <div className="flex flex-row flex-wrap gap-2">
+                    {IngredientCard({product: {name: "Product 1", description: "Description 1", imgSrc: "https://via.placeholder.com/150"}})}
+                    {IngredientCard({product: {name: "Product 2", description: "Description 2", imgSrc: "https://via.placeholder.com/150"}})}
+                    </div>
                 </div>
             </div>
         </div>
+    </>
     )
 }
